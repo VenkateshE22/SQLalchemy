4e0a46d0e9c346f9f1f9adff96997e52fada9f28
Venkatesh Eleswarapu <dell@abhijeet-Latitude-5480gmail.com>
2022-11-01 15:44:20 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime, Integer, create_engine
+from datetime import datetime
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+connection_string="sqlite:///"+os.path.join(BASE_DIR, 'site.db')
+Base = declarative_base()
+
+engine =create_engine(connection_string, echo=True)
+
+
+"""
+class User
+    id int
+    username str
+    email str
+    date_created datetime
+
+"""
+
+class User(Base):
+    __tablename__='users'
+    id=Column(Integer(), primary_key=True)
+    username=Column(String(30),nullable=False, unique=True)
+    email=Column(String(50), unique=True, nullable=False)
+    date_created=Column(DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User username={self.username} email={self.email}>"
+
+new_user=User(id=1,username="Venkatesh", email="evenkatesh@yahoo.com")
+print(new_user)
